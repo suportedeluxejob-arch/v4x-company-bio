f9a36d14b40e064223c3d911d6937e27fcb9a75c
suportedeluxejob-arch <suporte.deluxejob@gmail.com>
2026-02-09 12:03:11 -0300

    first

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, type 
 import { getAnalytics, isSupported } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ373oF_sQxP7iBRHyrlEzG5BY_eImYu8",
-  authDomain: "links-isabelle.firebaseapp.com",
-  databaseURL: "https://links-isabelle-default-rtdb.firebaseio.com",
-  projectId: "links-isabelle",
-  storageBucket: "links-isabelle.firebasestorage.app",
-  messagingSenderId: "558227177266",
-  appId: "1:558227177266:web:ac4153b44c94595603212b",
-  measurementId: "G-TT4JPSCN7Z"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase only once
